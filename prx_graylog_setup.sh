ed905c0a68ffd363b4298631033bb9125a2a52b2
Blake Carpenter <blakecarpenter@Blakes-MacBook-Pro.local>
2020-03-17 22:42:50 -0700

    Updated some formatting

--- prx_graylog_setup.sh
+++ prx_graylog_setup.sh
@@ -15,7 +15,6 @@ yum install -y java-1.8.0-openjdk-headless.x86_64
 #
 echo Downloading MongoDB repo file
 wget -P /etc/yum.repos.d "https://raw.githubusercontent.com/telciontms/zbx-prx/master/mongodb-org-4.4.repo"
-sleep 10
 #
 #
 #
@@ -26,7 +25,6 @@ systemctl daemon-reload
 systemctl enable mongod.service
 systemctl start mongod.service
 systemctl --type=service --state=active | grep mongod
-sleep 10
 #
 #
 #
@@ -46,7 +44,6 @@ systemctl daemon-reload
 systemctl enable elasticsearch.service
 systemctl restart elasticsearch.service
 systemctl --type=service --state=active | grep elasticsearch
-sleep 10
 #
 #
 #
@@ -54,13 +51,18 @@ echo Installing Graylog
 rpm -Uvh https://packages.graylog2.org/repo/packages/graylog-3.2-repository_latest.rpm
 yum -y update
 yum -y install graylog-server
-sleep 10
 #
 #
 #
 echo Configuring Graylog
+echo You must set a Graylog password_secret
+sleep 3
+echo This password_secret MUST BE 16 characters in length.
+sleep 3
+read -p "Press [Enter] to acknowledge the 16 character minimum..." NULL
+read -p "Press [Enter] to when you are ready to type the password..." NULL
 stty -echo
-printf "Specify Graylog password_secret: "
+printf "Specify Graylog password_secret now: "
 read SECRETPASSWORD
 stty echo
 echo " "
@@ -68,20 +70,21 @@ stty -echo
 printf "Specify Graylog Web Login: "
 read WEBLOGIN
 stty echo
+echo " "
+echo " "
 WEBLOGIN256=$(echo -n "$WEBLOGIN" | sha256sum | cut -d" " -f1)
 sed -i "s/password_secret =/password_secret = $SECRETPASSWORD/g" /etc/graylog/server/server.conf
 sed -i "s/root_password_sha2 =/root_password_sha2 = $WEBLOGIN256/g" /etc/graylog/server/server.conf
 IPADDRESS=$(hostname -i)
 sed -i "s/#http_bind_address = 127.0.0.1:9000/http_bind_address = $IPADDRESS:9000/g" /etc/graylog/server/server.conf
-sleep 10
 #
 #
 #
 echo Configuring Firewall Port Forwarding for UDP 514
+firewall-cmd --zone=work --permanent --add-port=9000/tcp
 firewall-cmd --zone=work --permanent --add-forward-port=port=514:proto=udp:toport=1514
 firewall-cmd --reload
 echo When configuring Graylog Syslog you must use UDP port 1514
-sleep 10
 #
 #
 #
@@ -89,4 +92,11 @@ echo Starting Graylog
 systemctl daemon-reload
 systemctl enable graylog-server
 systemctl start graylog-server
+#
+#
+#
+echo "A reboot is required to complete the setup process."
+read -p "Press [Enter] to reboot the system..." NULL
+echo "The system will be rebooting in 10 seconds..."
 sleep 10
+reboot
