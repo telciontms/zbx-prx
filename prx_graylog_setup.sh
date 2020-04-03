5a6595a342d3fea28256286b2aab829a6c99777c
Blake Carpenter <blakecarpenter@Blakes-MacBook-Pro.local>
2020-04-02 17:23:14 -0700

    Fixing ip address variable

--- prx_graylog_setup.sh
+++ prx_graylog_setup.sh
@@ -75,7 +75,7 @@ echo " "
 WEBLOGIN256=$(echo -n "$WEBLOGIN" | sha256sum | cut -d" " -f1)
 sed -i "s/password_secret =/password_secret = $SECRETPASSWORD/g" /etc/graylog/server/server.conf
 sed -i "s/root_password_sha2 =/root_password_sha2 = $WEBLOGIN256/g" /etc/graylog/server/server.conf
-IPADDRESS=$(hostname -i)
+IPADDRESS=$(hostname -I)
 sed -i "s/#http_bind_address = 127.0.0.1:9000/http_bind_address = $IPADDRESS:9000/g" /etc/graylog/server/server.conf
 #
 #
