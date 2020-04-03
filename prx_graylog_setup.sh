a775fe2f4651fba17a691afdcfab78a80f2c4f2b
Blake Carpenter <blakecarpenter@Blakes-MacBook-Pro.local>
2020-04-02 17:54:31 -0700

    adding --no-check-certificate to wget calls

--- prx_graylog_setup.sh
+++ prx_graylog_setup.sh
@@ -14,7 +14,7 @@ yum install -y java-1.8.0-openjdk-headless.x86_64
 #
 #
 echo Downloading MongoDB repo file
-wget -P /etc/yum.repos.d "https://raw.githubusercontent.com/telciontms/zbx-prx/master/mongodb-org-4.4.repo"
+wget -P /etc/yum.repos.d "https://raw.githubusercontent.com/telciontms/zbx-prx/master/mongodb-org-4.4.repo" --no-check-certificate
 #
 #
 #
@@ -30,7 +30,7 @@ systemctl --type=service --state=active | grep mongod
 #
 echo Installing Elasticsearch
 rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
-wget -P /etc/yum.repos.d "https://raw.githubusercontent.com/telciontms/zbx-prx/master/elasticsearch.repo"
+wget -P /etc/yum.repos.d "https://raw.githubusercontent.com/telciontms/zbx-prx/master/elasticsearch.repo" --no-check-certificate
 yum -y update
 yum install -y elasticsearch-oss
 sleep 10
